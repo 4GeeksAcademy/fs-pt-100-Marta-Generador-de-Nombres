554db43330ded09ecf36f94a80a08aea394cf7a4
Marta <159387209+martaic@users.noreply.github.com>
2025-03-14 11:55:09 +0000

    generador

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our', 'his', 'their'];
+  let adj = ['great', 'big', 'amazing', 'silly'];
+  let noun = ['jogger', 'racoon', 'uncle', 'brother'];
+  const dominio = ['.com', '.net', '.us', '.io'];
+
+  pronoun.forEach(pro => {
+    adj.forEach(ad => {
+      noun.forEach(no => {
+        dominio.forEach(dom => {
+          console.log(`${pro}${ad}${no}${dom}`);
+        });
+      });
+    });
+  });
 };
